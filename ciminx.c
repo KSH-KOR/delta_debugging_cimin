fbbb59460ffa29701ec7785a95dbed3b3571a965
kim <kimcoding0704@gmail.com>
2023-04-08 10:43:25 +0900

    jsmn done

--- ciminx.c
+++ ciminx.c
@@ -17,8 +17,8 @@
 #define STD_IN 0
 
 int does_contain_error_msg(char *error_msg);
-int child_process();
-int parent_process(char *test);
+int child_process(char *test);
+void parent_process();
 int save_in_file(char *target, char *filename);
 int execute_target_program();
 char *reduce(char *t);
@@ -39,7 +39,7 @@ int child_pid;
 
 int does_contain_error_msg(char *error_msg)
 {
-    printf("error msg::\n%s\n", error_msg);
+    // printf("error msg::\n%s\n", error_msg);
     return strstr(error_msg, error_string) != NULL;
 }
 int save_in_file(char *target, char *filename)
@@ -56,7 +56,7 @@ int save_in_file(char *target, char *filename)
 }
 int execute_target_program()
 {
-    printf("Launce exec process with PID: %d. Target program :%s\n",child_pid, program_args[0]);
+    // printf("Launce exec process with PID: %d. Target program :%s\n", child_pid, program_args[0]);
     int flag;
     if (program_args[1] == NULL)
     {
@@ -64,9 +64,10 @@ int execute_target_program()
     }
     else
     {
-        for(int i=1; program_args[i] != NULL; i++){
-            printf("program %d args: %s\n", i, program_args[i]);
-        }
+        // for (int i = 1; program_args[i] != NULL; i++)
+        // {
+        //     printf("program %d args: %s\n", i, program_args[i]);
+        // }
         flag = execv(program_args[0], &(program_args[1]));
     }
     return flag;
@@ -114,69 +115,79 @@ int read_input_from_file()
     free(buf);
     return 0;
 }
+void handle_alarm(int sig)
+{
+    printf("child process is not responding... terminate all processes\n");
+    kill(child_pid, SIGINT);
+    exit(0);
+}
 void handle_sigint(int sig)
 {
     if (child_pid > 0)
     {
         kill(child_pid, SIGKILL);
     }
-    char buffer[BUFFER_SIZE];
-    lseek(pipe_fd[0], 0, SEEK_SET);
-    int num_read = read(pipe_fd[0], buffer, BUFFER_SIZE - 1);
-    buffer[num_read] = '\0';
-    printf("Shortest crashing input size: %d\n", num_read);
-    printf("Output: %s", buffer);
+    // printf("Shortest crashing input size: %d\n", num_read);
+    printf("Output: %s", reduced_input);
     close(pipe_fd[0]);
     close(pipe_fd[1]);
     close(pipe_err_fd[0]);
     close(pipe_err_fd[1]);
     exit(0);
 }
-void close_read_end_pipe(int *which_pipe){
+void close_read_end_pipe(int *which_pipe)
+{
     close(which_pipe[0]);
 }
-void close_write_end_pipe(int *which_pipe){
+void close_write_end_pipe(int *which_pipe)
+{
     close(which_pipe[1]);
 }
 // note: it does not close the write end of pipe after the write.
-void write_to_pipe(char *to_write, int *which_pipe){
+void write_to_pipe(char *to_write, int *which_pipe)
+{
     ssize_t to_sent = strlen(to_write);
-    char end = '\0';
     ssize_t sent_so_far = 0;
-    printf("start wrting to fd: %d...\ndata size: %ld", which_pipe[1], to_sent);
-    while(sent_so_far < to_sent){
-        ssize_t sent;
-        if(sent = write(which_pipe[1], to_write, to_sent) == -1){
-            fprintf(stderr, "Write to pipe error: %s\n", strerror(errno));
-        }
-        printf("just wrote: %ld, wrote so far: %ld\n", sent, sent_so_far);
-        sent_so_far+=sent;
-        break;
-    }
-    printf("done writing to fd: %d\nwrote: data size: %ld\n", which_pipe[1], sent_so_far);
-}
-// note: it does not close the read end of pipe after the read.
-void read_from_pipe(char *buffer, int *which_pipe){
-    int bytes_read;
-    while ((bytes_read = read(which_pipe[0], buffer, BUFFER_SIZE - 1)) > 0)
+    // printf("start wrting to fd: %d...\ndata size: %ld\n", which_pipe[1], to_sent);
+    ssize_t sent = 0;
+    while (sent_so_far < to_sent)
     {
-        buffer[bytes_read+1] = 0x0;
+        if ((sent = write(which_pipe[1], to_write + sent_so_far, to_sent - sent_so_far)) <= 0)
+        {
+            fprintf(stderr, "Write to pipe error: %s\ntried to write this: \n%s\n", strerror(errno), to_write);
+            exit(0);
+        }
+        // printf("just wrote: %ld, wrote so far: %ld\n", sent, sent_so_far);
+        sent_so_far += sent;
     }
+
+    // printf("done writing to fd: %d\nwrote: data size: %ld\n", which_pipe[1], sent_so_far);
 }
 
-int child_process()
+// note: it does not close the read end of pipe after the read.
+void read_from_pipe(char *buffer, int *which_pipe)
+{
+    int bytes_read;
+    bytes_read = read(which_pipe[0], buffer, BUFFER_SIZE - 1);
+    buffer[bytes_read + 1] = '\0';
+}
+
+int child_process(char *test)
 {
     if (dup2(pipe_err_fd[PIPE_ERR_WRITE], STD_ERR) == -1)
     {
         fprintf(stderr, "Error err dup2: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
     }
     if (dup2(pipe_fd[PIPE_READ], STD_IN) == -1)
     {
         fprintf(stderr, "Error dup2: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
     }
-    close_write_end_pipe(pipe_err_fd);
-    close_read_end_pipe(pipe_fd);
-    //close(STDOUT_FILENO); // dont listen what the target program says through stdout fd
+    write_to_pipe(test, pipe_fd);
+    close_write_end_pipe(pipe_fd);
+    close_read_end_pipe(pipe_err_fd);
+    close(STDOUT_FILENO); // dont listen what the target program says through stdout fd
     int flag = execute_target_program();
     if (flag == -1)
     {
@@ -185,64 +196,90 @@ int child_process()
     }
 }
 
-int parent_process(char *test)
+void parent_process()
 {
     close_read_end_pipe(pipe_fd);
     close_write_end_pipe(pipe_err_fd);
-
-    write_to_pipe(test, pipe_fd);
     close_write_end_pipe(pipe_fd);
-    printf("done writing!\n");
-    
-    // read from pipe
-    char buffer[BUFFER_SIZE];
-    read_from_pipe(buffer, pipe_err_fd);
-    close_read_end_pipe(pipe_err_fd);
-    if(does_contain_error_msg(buffer)){
-        printf("error msg found. kill child proc");
-        kill(child_pid, SIGINT);
-        return 0;
-    }
-
-    fprintf(stderr, "Error string not found in output\n");
-    // error string not found
-    return -1;
 }
-int c = 0;
-// 𝑝, a target programt that crashes p
-// 𝑐, a predicate over output to check if the program crashed
+
+void one_cycle(char *tm)
+{
+    child_pid = fork();
+    if (child_pid == 0)
+    {
+        child_process(tm);
+    }
+    // the child process won't reach here if it is alright.
+    // wait for the child process to write error in the parent process.
+    int status;
+    waitpid(child_pid, &status, 0);
+    if (WIFEXITED(status))
+    {
+        printf("child process terminated with the status: %d\n", WIFEXITED(status));
+    }
+}
+
 char *reduce(char *t)
 {
     char *tm = t;
     int s = strlen(tm) - 1;
     while (s > 0)
     {
+        if(reduced_input != NULL) {
+            printf("%s\n", reduced_input);
+        }
+            
         for (int i = 0; i < strlen(tm) - s; i++)
         {
+            // create pipe
+            if (pipe(pipe_fd) == -1 || pipe(pipe_err_fd) == -1)
+            {
+                fprintf(stderr, "Error creating pipe: %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
             char *head = strndup(tm, i); // The strndup(s, n) function copies at most n bytes. If s is longer than n, only n bytes are copied, and a terminating null byte('\0') is added.
             char *tail = strndup(&tm[i + s], strlen(tm) - 1);
             char *test = malloc(strlen(head) + strlen(tail) + 1);
             strcpy(test, head);
             strcat(test, tail);
-
+            //printf("%s\n", test);
             child_pid = fork();
             if (child_pid == 0)
             { // child process
-                child_process();
+                alarm(3);
+                child_process(test);
             }
             else
             { // parent process
-                if (parent_process(test) == 0)
-                {
-                    printf("1\n");
-                    char *reduced = reduce(test);
-                    free(test);
-                    free(head);
-                    free(tail);
-                    return reduced;
-                }
+                parent_process();
+            }
+            // read from pipe
+            char buffer[BUFFER_SIZE];
+            int status;
+            waitpid(child_pid, &status, 0);
+            if (WIFEXITED(status))
+            {
+                //printf("child process terminated with the status: %d\n", WIFEXITED(status));
+                alarm(0);
+            }
+            read_from_pipe(buffer, pipe_err_fd);
+
+            close_read_end_pipe(pipe_err_fd);
+            
+            if (does_contain_error_msg(buffer))
+            {
+                kill(child_pid, SIGINT);
+                char *reduced = reduce(test);
+                reduced_input = reduced;
+                // free(test);
+                // free(head);
+                // free(tail);
+                return reduced;
             }
 
+            // fprintf(stderr, "Error string not found in output\n");
+            //  error string not found
             free(test);
             free(head);
             free(tail);
@@ -250,24 +287,45 @@ char *reduce(char *t)
 
         for (int i = 0; i < strlen(tm) - s; i++)
         {
+            if (pipe(pipe_fd) == -1 || pipe(pipe_err_fd) == -1)
+            {
+                fprintf(stderr, "Error creating pipe: %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
             char *mid = strndup(&tm[i], s);
-
+            //printf("%s\n", mid);
             child_pid = fork();
             if (child_pid == 0)
             { // child process
-                child_process();
+                alarm(3);
+                child_process(mid);
             }
             else
             { // parent process
-                if (parent_process(mid) == 0)
-                {
-                    printf("2\n");
-                    char *reduced = reduce(mid);
-                    free(mid);
-                    return reduced;
-                }
+                parent_process();
+            }
+            // read from pipe
+            char buffer[BUFFER_SIZE];
+            int status;
+            waitpid(child_pid, &status, 0);
+            if (WIFEXITED(status))
+            {
+                //printf("child process terminated with the status: %d\n", WIFEXITED(status));
+                alarm(0);
+            }
+            read_from_pipe(buffer, pipe_err_fd);
+
+            close_read_end_pipe(pipe_err_fd);
+            if (does_contain_error_msg(buffer))
+            {
+                kill(child_pid, SIGINT);
+                char *reduced = reduce(mid);
+                reduced_input = reduced;
+                // free(mid);
+                return reduced;
             }
 
+            // fprintf(stderr, "Error string not found in output\n");
             free(mid);
         }
         s--;
@@ -282,7 +340,6 @@ char *minimize(char *t)
 
 int main(int argc, char *argv[])
 {
-    printf("Hello World\n");
     // parse command-line arguments
     int opt;
     while ((opt = getopt(argc, argv, "i:m:o:")) != -1)
@@ -302,7 +359,8 @@ int main(int argc, char *argv[])
             fprintf(stderr, "Usage: %s -i input_path -m error_string -o output_path\n", argv[0]);
             exit(EXIT_FAILURE);
         }
-        if(input_file_path != NULL && error_string != NULL && output_file_path != NULL) break;
+        if (input_file_path != NULL && error_string != NULL && output_file_path != NULL)
+            break;
     }
 
     // check if required arguments are provided
@@ -311,66 +369,29 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: %s -i input_path -m error_string -o output_path\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    
+
     program_args = &argv[optind]; // assume all the option arguments are handled.
-    
+
+    signal(SIGALRM, handle_alarm);
+    signal(SIGINT, handle_sigint);
+
     printf("\n--------------------------------\n");
     printf("input_path: %s\n", input_file_path);
     printf("error_string: %s\n", error_string);
     printf("output_path: %s\n", output_file_path);
     printf("target program path: %s\n", program_args[0]);
-    for(int i=0; program_args[i] != NULL; i++){
+    for (int i = 0; program_args[i] != NULL; i++)
+    {
         printf("\n program arg: %s\n", program_args[i]);
     }
     printf("--------------------------------\n");
-    
-    // create pipe
-    if (pipe(pipe_fd) == -1 || pipe(pipe_err_fd) == -1)
-    {
-        fprintf(stderr, "Error creating pipe: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
 
     read_input_from_file();
- 
+
     reduced_input = minimize(original_input);
 
+    printf("result\n%s\n", reduced_input);
     save_in_file(reduced_input, output_file_path);
     printf("Bye!\n");
     return 0;
 }
-
-// int test(){
-//     // Set up the select call to monitor the pipe for readability
-//     fd_set readfds;
-//     FD_ZERO(&readfds);
-//     FD_SET(pipe_err_fd[PIPE_ERR_READ], &readfds);
-
-//     struct timeval timeout_val;
-//     timeout_val.tv_sec = 3;
-//     timeout_val.tv_usec = 0;
-
-//     int result = select(pipe_err_fd[PIPE_ERR_READ] + 1, &readfds, NULL, NULL, &timeout_val);
-//     if (result == -1)
-//     {
-//         perror("Failed to wait for pipe");
-//         exit(EXIT_FAILURE);
-//     }
-//     else if (result == 0)
-//     {
-//         printf("Timeout waiting for data on the pipe. Sending SIGKILL to Excel process...\n");
-//         // Forcefully terminate the Excel process with SIGKILL
-//         if (kill(child_pid, SIGKILL) == 0)
-//         {
-//             printf("Sent SIGKILL to Excel process with PID \n");
-//             return 1;
-//         }
-//         else
-//         {
-//             perror("Failed to send SIGKILL");
-//             exit(EXIT_FAILURE);
-//         }
-//     }
-//     else
-//     {}
-// }
